60f9d5addfde397f19a2e5b0e66daecd94044d2e
Varun-K34 <appuvarun88@gmail.com>
2025-01-25 13:09:05 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/techqrmaintance/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.techqrmaintance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 309,
+            width: 428,
+            color: Colors.amber,
+          ),
+        ],
+      ),
+    );
+  }
+}
