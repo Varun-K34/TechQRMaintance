fbe7a82a3dec7864f37a2688f48776bf0ac08604
thegreatcode1 <suhaibpa72@gmail.com>
2025-01-26 10:33:37 +0530

    code formated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'Screens/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:techqrmaintance/Screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginScreen()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const LoginScreen());
   }
 }
-
