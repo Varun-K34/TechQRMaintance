fcc9143e609a4ebe7fbc1443efb34cfe22474453
thegreatcode1 <suhaibpa72@gmail.com>
2025-02-03 16:06:56 +0530

    Refactor ServicesHistoryScreen and TaskScreen to integrate Complaintbloc for state management; remove search text field

--- lib/Screens/history/services_history.dart
+++ lib/Screens/history/services_history.dart
@@ -1,14 +1,20 @@
-import 'dart:developer';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:techqrmaintance/Screens/Widgets/table_widget.dart';
+import 'package:techqrmaintance/application/bloccomplaint/complaintbloc_bloc.dart';
 
 class ServicesHistoryScreen extends StatelessWidget {
   const ServicesHistoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) {
+        context
+            .read<ComplaintblocBloc>()
+            .add(ComplaintblocEvent.getComplaintsTasks());
+      },
+    );
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -24,21 +30,49 @@ class ServicesHistoryScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CupertinoSearchTextField(
-              onChanged: (value) {
-                // Handle search input changes
-                log("Search text: $value");
-              },
-              onSubmitted: (value) {
-                // Handle search submission
-                log("Submitted text: $value");
-              },
-              placeholder: "Search tasks...",
-            ),
-          ),
-          Expanded(child: TaskTable(title: "History", rowCount: 20)),
+          Expanded(child: BlocBuilder<ComplaintblocBloc, ComplaintblocState>(
+            builder: (context, state) {
+              if (state.isLoading) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state.complaints.isEmpty) {
+                return Center(
+                  child: Text(
+                    "No tasks found",
+                    style: TextStyle(
+                      color: Color(0xff165069),
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else if (state.isFailure) {
+                return Center(
+                  child: Text(
+                    "Oops! Something went wrong. Please try again later.",
+                    style: TextStyle(
+                      color: Color(0xff165069),
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              }
+              return TaskTable(
+                statevalues: state.complaints
+                    .where((task) => task.status == "completed")
+                    .toList(),
+                title: "History",
+                rowCount: state.complaints
+                    .where((task) => task.status == "completed")
+                    .toList()
+                    .length,
+              );
+            },
+          )),
         ],
       ),
     );
