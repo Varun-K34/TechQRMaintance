f0a7339ed738ff51f5f05e4c3f45fff799ff5584
thegreatcode1 <suhaibpa72@gmail.com>
2025-02-08 14:10:03 +0530

    Remove unused imports in task overview and task screen files for cleaner code

--- lib/Screens/tasks/task_screen.dart
+++ lib/Screens/tasks/task_screen.dart
@@ -1,4 +1,3 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:techqrmaintance/Screens/Widgets/table_widget.dart';
