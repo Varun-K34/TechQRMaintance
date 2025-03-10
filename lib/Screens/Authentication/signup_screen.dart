2516018fbaca357e9a5831f254e071bc834e051c
thegreatcode1 <suhaibpa72@gmail.com>
2025-03-10 13:53:50 +0530

    Add DropDownSearchWidget for organization selection; implement UI and interaction logic

--- lib/Screens/Authentication/signup_screen.dart
+++ lib/Screens/Authentication/signup_screen.dart
@@ -1,30 +1,29 @@
 import 'dart:developer';
 
-import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:techqrmaintance/Screens/Widgets/custom_button.dart';
 import 'package:techqrmaintance/Screens/Widgets/custom_textfield.dart';
+import 'package:techqrmaintance/Screens/Widgets/drop_down_widget.dart';
 import 'package:techqrmaintance/Screens/Widgets/snakbar_widget.dart';
 import 'package:techqrmaintance/application/authbloc/authbloc_bloc.dart';
 import 'package:techqrmaintance/application/orgganizationbloc/oranization_bloc.dart';
 import 'package:techqrmaintance/core/colors.dart';
 import 'package:techqrmaintance/domain/authregmodel/model/auth_reg_model.dart';
-import 'package:techqrmaintance/domain/organizationmodel/organization_repo.dart';
 
 class SignupScreen extends StatelessWidget {
+  final TextEditingController orgidcontroller = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
       TextEditingController();
-      
+
   SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    String orgIds = "";
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context
           .read<OranizationBloc>()
@@ -54,40 +53,16 @@ class SignupScreen extends StatelessWidget {
           //dropdown search
           BlocBuilder<OranizationBloc, OranizationState>(
             builder: (context, state) {
-               if (state.isFailure) {
-                 CustomSnackbar.shows(context, message: "somting went wrong");
-               }
-
-              return DropdownSearch(
-                items: (filter, loadProps) {
-                  return state.organizationList.data
-                          ?.map((e) => "(${e.id}) ${e.orgName}")
-                          .toList() ??
-                      [];
-                },
-                onChanged: (value) {
-                final orgId = value.toString().split(")")[0].split("(")[1];
-                orgIds= orgId;
-                  log(orgIds);
-                },
-               popupProps: PopupProps.menu(
-                  showSearchBox: true,
-                  searchFieldProps: TextFieldProps(
-                    decoration: InputDecoration(
-                      labelText: "Search organization",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                decoratorProps: DropDownDecoratorProps(
-                  decoration: InputDecoration(
-                    labelText: "organization",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                compareFn: (item1, item2) {
-                  return item1 == item2;
-                },
+              if (state.isFailure) {
+                CustomSnackbar.shows(context, message: "somting went wrong");
+              }
+              return DropDownSearchWidget(
+                key: Key("org"),
+                orgidcontroller: orgidcontroller,
+                states: state.organizationList.data
+                ?.map((e) => "(${e.id}) ${e.orgName}")
+                .toList() ??
+            [],
               );
             },
           ),
@@ -122,62 +97,69 @@ class SignupScreen extends StatelessWidget {
             curveRadius: 30,
             boolVal: false,
           ),
-          BlocListener<AuthblocBloc, AuthblocState>(listener: (context, state) {
-            if (state.isError) {
-              CustomSnackbar.shows(context,
-                  message: "Oops! Something went wrong. Please try again.");
-            }
-          }, child: BlocBuilder<AuthblocBloc, AuthblocState>(
-            builder: (context, state) {
-              return state.isloading == true
-                  ? CircularProgressIndicator()
-                  : CustomMaterialButton(
-                      text: "Sign Up",
-                      onPressed: () {
-                        final String username = _usernameController.text.trim();
-                        final String email = _emailController.text.trim();
-                        final String password = _passwordController.text.trim();
-                        if (username.isEmpty &&
-                            email.isEmpty &&
-                            password.isEmpty&&orgIds.isEmpty) {
-                          CustomSnackbar.shows(context,
-                              message:
-                                  "Please fill in all the fields to proceed");
-                          return;
-                        } else if (username.isEmpty) {
-                          CustomSnackbar.shows(context,
-                              message: "Username cannot be empty");
-                          return;
-                        } else if (email.isEmpty) {
-                          CustomSnackbar.shows(context,
-                              message: "Email cannot be empty");
-                          return;
-                        } else if (password.isEmpty) {
-                          CustomSnackbar.shows(context,
-                              message: "Password cannot be empty");
-                          return;
-                        }
-                        final model = AuthRegModel(
-                          orgId: int.parse(orgIds),
-                          full_name: username,
-                          email: email,
-                          password: password,
-                        );
-                        log(model.toJson().toString());
-                        context.read<AuthblocBloc>().add(
-                              AuthblocEvent.signup(
-                                authmodel: model,
-                              ),
-                            );
-                      },
-                    );
+          BlocListener<AuthblocBloc, AuthblocState>(
+            listener: (context, state) {
+              if (state.isError) {
+                CustomSnackbar.shows(context,
+                    message: "Oops! Something went wrong. Please try again.");
+              }
             },
-          )),
+            child: BlocBuilder<AuthblocBloc, AuthblocState>(
+              builder: (context, state) {
+                return state.isloading == true
+                    ? CircularProgressIndicator()
+                    : CustomMaterialButton(
+                        text: "Sign Up",
+                        onPressed: state.isloading
+                            ? () {}
+                            : () => onPressedSignup(context),
+                      );
+              },
+            ),
+          ),
           SizedBox(
             height: 20,
           ),
         ],
       ),
     );
+  }
+
+  void onPressedSignup(BuildContext context) {
+    final String username = _usernameController.text.trim();
+    final String email = _emailController.text.trim();
+    final String password = _passwordController.text.trim();
+    log(password.length.toString());
+    final String orgid = orgidcontroller.text.trim();
+    if (username.isEmpty) {
+      CustomSnackbar.shows(context, message: "Username cannot be empty");
+      return;
+    } else if (email.isEmpty) {
+      CustomSnackbar.shows(context, message: "Email cannot be empty");
+      return;
+    } else if (password.isEmpty) {
+      log("password is empty", name: "password");
+      CustomSnackbar.shows(context, message: "Password cannot be empty.");
+      return;
+    } else if (orgid.isEmpty) {
+      CustomSnackbar.shows(context, message: "Organization cannot be empty");
+      return;
+    } else if (password.length < 8) {
+      CustomSnackbar.shows(context,
+          message: "Password must be at least 8 characters");
+      return;
+    }
+    final model = AuthRegModel(
+      orgId: int.parse(orgid),
+      full_name: username,
+      email: email,
+      password: password,
+    );
+    log(model.toJson().toString());
+    context.read<AuthblocBloc>().add(
+          AuthblocEvent.signup(
+            authmodel: model,
+          ),
+        );
   }
 }
