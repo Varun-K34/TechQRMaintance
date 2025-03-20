412658732599d1397f98865fa78f8660aef53c20
thegreatcode1 <suhaibpa72@gmail.com>
2025-03-20 16:52:34 +0530

    no change

--- lib/Screens/tasks/task_screen.dart
+++ lib/Screens/tasks/task_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:techqrmaintance/Screens/Widgets/skelton.dart';
+import 'package:techqrmaintance/Screens/Widgets/snakbar_widget.dart';
 import 'package:techqrmaintance/application/servicesrequest/service_request_bloc.dart';
 import 'package:techqrmaintance/application/spbloc/spbloc_bloc.dart';
 import 'package:techqrmaintance/core/colors.dart';
@@ -36,126 +37,146 @@ class TaskScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: BlocBuilder<ServiceRequestBloc, ServiceRequestState>(
-        builder: (context, state) {
-          if (state.isLoading) {
-            ListView(
-              children: List.generate(
-                10,
-                (index) {
-                  return TweenAnimationBuilder(
-                    tween: Tween<double>(begin: 0.0, end: 1.0),
-                    duration: Duration(milliseconds: 500 + (index * 100)),
-                    builder: (context, double value, child) {
-                      return Opacity(
-                        opacity: value,
-                        child: Transform.scale(
-                          scale: value,
-                          child: Skeleton(
-                            height: 100,
-                            width: double.infinity,
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            );
-          }
-          return ListView(
-            children: List.generate(
-              state.servicelist.length,
-              (index) {
-                final ServicesModel services = state.servicelist[index];
-                return TweenAnimationBuilder(
-                  tween: Tween<double>(begin: 0.0, end: 1.0),
-                  duration: Duration(milliseconds: 500),
-                  builder: (context, double value, child) {
-                    return Opacity(
-                      opacity: value,
-                      child: Transform.translate(
-                        offset: Offset(0, 50 * (1 - value)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 10,
-                            vertical: 10,
-                          ),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(30),
-                              color: const Color(0xff165069),
-                            ),
-                            child: Column(
-                              children: [
-                                Container(
-                                  width: double.infinity,
-                                  height: 150,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(30),
-                                        topRight: Radius.circular(30)),
-                                    color: Color(0xFFF5F5F5),
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: NetworkImage(
-                                        ac,
-                                        scale: 100,
+      body: BlocBuilder<SpblocBloc, SpblocState>(
+        builder: (context, spstate) {
+          return BlocBuilder<ServiceRequestBloc, ServiceRequestState>(
+            builder: (context, state) {
+              if (state.isFailure) {
+                CustomSnackbar.shows(
+                  context,
+                  message:
+                      "Oops! Something went wrong. Please try again later.",
+                );
+              }
+              return state.isLoading
+                  ? ListView(
+                      children: List.generate(
+                        10,
+                        (index) {
+                          return TweenAnimationBuilder(
+                            tween: Tween<double>(begin: 0.0, end: 1.0),
+                            duration:
+                                Duration(milliseconds: 500 + (index * 100)),
+                            builder: (context, double value, child) {
+                              return Opacity(
+                                opacity: value,
+                                child: Transform.scale(
+                                  scale: value,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10),
+                                    child: Skeleton(
+                                      height: 300,
+                                      width: double.infinity,
+                                      color: primaryWhite,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    )
+                  : ListView(
+                      children: List.generate(
+                        state.servicelist.length,
+                        (index) {
+                          final ServicesModel services =
+                              state.servicelist[index];
+                          return TweenAnimationBuilder(
+                            tween: Tween<double>(begin: 0.0, end: 1.0),
+                            duration: Duration(milliseconds: 500),
+                            builder: (context, double value, child) {
+                              return Opacity(
+                                opacity: value,
+                                child: Transform.translate(
+                                  offset: Offset(0, 50 * (1 - value)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 10,
+                                      vertical: 10,
+                                    ),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(30),
+                                        color: const Color(0xff165069),
+                                      ),
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            width: double.infinity,
+                                            height: 150,
+                                            decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.only(
+                                                  topLeft: Radius.circular(30),
+                                                  topRight:
+                                                      Radius.circular(30)),
+                                              color: Color(0xFFF5F5F5),
+                                              image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: NetworkImage(
+                                                  ac,
+                                                  scale: 100,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 5,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 20, bottom: 10),
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  "Job Type : ${services.jobType}",
+                                                  style: TextStyle(
+                                                    color: primaryWhite,
+                                                    fontSize: 22,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                                TaskBuildRow(
+                                                  icon: Icons.devices,
+                                                  text:
+                                                      "${services.device?.category?.name}",
+                                                ),
+                                                TaskBuildRow(
+                                                    icon: Icons.person,
+                                                    text:
+                                                        "${services.customer?.fullName}"),
+                                                TaskBuildRow(
+                                                    icon: Icons.pending_actions,
+                                                    text: "${services.status}"),
+                                                TaskBuildRow(
+                                                    icon: Icons.access_time,
+                                                    text:
+                                                        "${services.preferredTimeslot}"),
+                                                TaskBuildRow(
+                                                    icon: Icons.flag,
+                                                    text:
+                                                        "${services.emergency}" ==
+                                                                "1"
+                                                            ? "High priority"
+                                                            : "Low priority"),
+                                              ],
+                                            ),
+                                          )
+                                        ],
                                       ),
                                     ),
                                   ),
                                 ),
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 20, bottom: 10),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "Job Type : ${services.jobType}",
-                                        style: TextStyle(
-                                          color: primaryWhite,
-                                          fontSize: 22,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      TaskBuildRow(
-                                        icon: Icons.devices,
-                                        text:
-                                            "${services.device?.category?.name}",
-                                      ),
-                                      TaskBuildRow(
-                                          icon: Icons.person,
-                                          text:
-                                              "${services.customer?.fullName}"),
-                                      TaskBuildRow(
-                                          icon: Icons.pending_actions,
-                                          text: "${services.status}"),
-                                      TaskBuildRow(
-                                          icon: Icons.access_time,
-                                          text:
-                                              "${services.preferredTimeslot}"),
-                                      TaskBuildRow(
-                                          icon: Icons.flag,
-                                          text: "${services.emergency}" == "1"
-                                              ? "High priority"
-                                              : "Low priority"),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
+                              );
+                            },
+                          );
+                        },
                       ),
                     );
-                  },
-                );
-              },
-            ),
+            },
           );
         },
       ),
